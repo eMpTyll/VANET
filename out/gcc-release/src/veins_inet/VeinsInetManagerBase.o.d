44f562f1348063769624df3c891786c96b9f56f9
THackVN <tungdeptrai2607@gmail.com>
2023-12-05 02:00:41 +0700

    Upload

---
+++ out/gcc-release/src/veins_inet/VeinsInetManagerBase.o.d
@@ -0,0 +1,113 @@
+../out/gcc-release/src/veins_inet/VeinsInetManagerBase.o: \
+ veins_inet/VeinsInetManagerBase.cc veins_inet/VeinsInetManagerBase.h \
+ veins_inet/veins_inet.h \
+ D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/veins.h \
+ D:/StudyTools/omnetpp-6.0pre14/omnetpp-6.0pre14/include/omnetpp.h \
+ D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/common/INETDefs.h \
+ D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/common/precompiled_release.h \
+ D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/common/precompiled.h \
+ D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/opp_defines.h \
+ D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/features.h \
+ D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/common/Compat.h \
+ D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/common/InitStages.h \
+ D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIScenarioManager.h \
+ D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/base/utils/Coord.h \
+ D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/base/utils/FWMath.h \
+ D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/base/modules/BaseWorldUtility.h \
+ D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/base/connectionManager/BaseConnectionManager.h \
+ D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/base/utils/AntennaPosition.h \
+ D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/base/connectionManager/NicEntry.h \
+ D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/base/utils/Heading.h \
+ D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/utility/HasLogProxy.h \
+ D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/base/utils/FindModule.h \
+ D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/obstacle/ObstacleControl.h \
+ D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/obstacle/Obstacle.h \
+ D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/world/annotations/AnnotationManager.h \
+ D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/utility/BBoxLookup.h \
+ D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/obstacle/VehicleObstacleControl.h \
+ D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/base/utils/Move.h \
+ D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/obstacle/MobileHostObstacle.h \
+ D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIBuffer.h \
+ D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIConstants.h \
+ D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIColor.h \
+ D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIConnection.h \
+ D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCICoord.h \
+ D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCICoordinateTransformation.h \
+ D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/VehicleSignal.h \
+ D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/base/utils/EnumBitset.h \
+ D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIRegionOfInterest.h \
+ D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/utility/SignalManager.h \
+ veins_inet/VeinsInetMobility.h \
+ D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/mobility/base/MobilityBase.h \
+ D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/common/ModuleAccess.h \
+ D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/common/StringFormat.h \
+ D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/common/geometry/common/CanvasProjection.h \
+ D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/common/geometry/common/RotationMatrix.h \
+ D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/common/geometry/common/Coord.h \
+ D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/common/INETMath.h \
+ D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/common/geometry/common/EulerAngles.h \
+ D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/common/Units.h \
+ D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/common/geometry/common/Quaternion.h \
+ D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/mobility/contract/IMobility.h \
+ D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCICommandInterface.h \
+ D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/world/traci/trafficLight/TraCITrafficLightProgram.h \
+ D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/common/scenario/ScenarioManager.h \
+ D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/common/lifecycle/LifecycleController.h \
+ D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/common/lifecycle/ILifecycle.h \
+ D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/common/scenario/IScriptable.h
+veins_inet/VeinsInetManagerBase.h:
+veins_inet/veins_inet.h:
+D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/veins.h:
+D:/StudyTools/omnetpp-6.0pre14/omnetpp-6.0pre14/include/omnetpp.h:
+D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/common/INETDefs.h:
+D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/common/precompiled_release.h:
+D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/common/precompiled.h:
+D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/opp_defines.h:
+D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/features.h:
+D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/common/Compat.h:
+D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/common/InitStages.h:
+D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIScenarioManager.h:
+D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/base/utils/Coord.h:
+D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/base/utils/FWMath.h:
+D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/base/modules/BaseWorldUtility.h:
+D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/base/connectionManager/BaseConnectionManager.h:
+D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/base/utils/AntennaPosition.h:
+D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/base/connectionManager/NicEntry.h:
+D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/base/utils/Heading.h:
+D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/utility/HasLogProxy.h:
+D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/base/utils/FindModule.h:
+D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/obstacle/ObstacleControl.h:
+D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/obstacle/Obstacle.h:
+D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/world/annotations/AnnotationManager.h:
+D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/utility/BBoxLookup.h:
+D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/obstacle/VehicleObstacleControl.h:
+D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/base/utils/Move.h:
+D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/obstacle/MobileHostObstacle.h:
+D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIBuffer.h:
+D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIConstants.h:
+D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIColor.h:
+D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIConnection.h:
+D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCICoord.h:
+D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCICoordinateTransformation.h:
+D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/VehicleSignal.h:
+D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/base/utils/EnumBitset.h:
+D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIRegionOfInterest.h:
+D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/utility/SignalManager.h:
+veins_inet/VeinsInetMobility.h:
+D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/mobility/base/MobilityBase.h:
+D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/common/ModuleAccess.h:
+D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/common/StringFormat.h:
+D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/common/geometry/common/CanvasProjection.h:
+D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/common/geometry/common/RotationMatrix.h:
+D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/common/geometry/common/Coord.h:
+D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/common/INETMath.h:
+D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/common/geometry/common/EulerAngles.h:
+D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/common/Units.h:
+D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/common/geometry/common/Quaternion.h:
+D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/mobility/contract/IMobility.h:
+D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCICommandInterface.h:
+D:/NCKH/VANET/veins-5.2/veins-veins-5.2/src/veins/modules/world/traci/trafficLight/TraCITrafficLightProgram.h:
+D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/common/scenario/ScenarioManager.h:
+D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/common/lifecycle/LifecycleController.h:
+D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/common/lifecycle/ILifecycle.h:
+D:/NCKH/VANET/inet-4.3.5/inet-4.3.5/src/inet/common/scenario/IScriptable.h:
